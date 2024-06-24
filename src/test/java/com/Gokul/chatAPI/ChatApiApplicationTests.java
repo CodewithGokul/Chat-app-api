9c775e65d4b5360253c4692444e882378a3f1ed1
CodewithGokul <gokulvijayuph@gmail.com>
2024-06-24 17:55:39 +0530

    chat-api initialized

---
+++ src/test/java/com/Gokul/chatAPI/ChatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gokul.chatAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
