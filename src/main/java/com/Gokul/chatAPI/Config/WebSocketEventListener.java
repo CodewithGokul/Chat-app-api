9fbaf0201601e9d036b240e771e9bbdb45d52a38
CodewithGokul <gokulvijayuph@gmail.com>
2024-06-28 15:55:55 +0530

    WebSocket Configuration Added

---
+++ src/main/java/com/Gokul/chatAPI/Config/WebSocketEventListener.java
@@ -0,0 +1,16 @@
+package com.Gokul.chatAPI.Config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+public class WebSocketEventListener {
+
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event){
+        e
+    }
+}
+
+
+
+
